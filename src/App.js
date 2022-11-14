f1c03a6a7cf04dff03afd3888af31f8fcbd062f2
Birute1969 <107874553+Birute1969@users.noreply.github.com>
2022-11-14 07:59:12 +0200

    Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
+
+import HomePage from './views/HomePage/HomePage';
+import ContactsPage from './views/ContactsPage/ContactsPage';
+import AboutPage from './views/AboutPage/AboutPage';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Routes>
     </div>
   );
 }
